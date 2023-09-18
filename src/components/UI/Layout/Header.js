32fbea6cd12fe64d0bd8cfac3f919ff36572de9f
otvertka <otvertka123@ukr.net>
2023-09-18 13:52:47 +0300

    first commit

---
+++ src/components/UI/Layout/Header.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header>
+        <h1>Header Name</h1>
+        <button>BUTTON</button>
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
