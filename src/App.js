ac472b660f0c3344e16650dbdd6da319102f3f88
otvertka <otvertka123@ukr.net>
2023-09-20 15:04:42 +0300

    header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Header from "./components/UI/Layout/Header";
 
 function App() {
   return (
-    <>
+    <div className="max-w-screen-xl mx-auto">
       <Header />
-    </>
+    </div>
   );
 }
 
--- src/components/UI/Layout/Header.jsx
+++ src/components/UI/Layout/Header.jsx
@@ -1,23 +1,20 @@
 import { Fragment } from "react";
 
+import mainLogo from "../images/header_images/main_logo.png";
+
 const Header = (props) => {
   return (
     <Fragment>
-      <header className="bg-black">
+      <header className="bg-black flex items-center justify-between">
         <a href="#">
-          {" "}
-          <img
-            src="C:\Users\JK\react-project\src\svg\logo.svg"
-            alt="logo"></img>
+          <img src={mainLogo} alt="logo"></img>
         </a>
-        <nav>
+        <nav className="text-white flex justify-between">
           {/* основные */}
-          <div className="text-white-100">
-            <ul>
+          <div>
+            <ul className="flex px-4">
               <li>
-                <a className="text-white-100" href="#">
-                  Trade In
-                </a>
+                <a href="#">Trade In</a>
               </li>
               <li>
                 <a href="#">Велосипеды</a>
@@ -38,18 +35,74 @@ const Header = (props) => {
           </div>
           {/* поиск, логин, избранное, корзина */}
           <div>
-            <ul>
+            <ul className="flex">
               <li>
-                <a href="#">1</a>
+                <a href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                    />
+                  </svg>
+                </a>
               </li>
               <li>
-                <a href="#">2</a>
+                <a href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </a>
               </li>
               <li>
-                <a href="#">3</a>
+                <a href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                    />
+                  </svg>
+                </a>
               </li>
               <li>
-                <a href="#">4</a>
+                <a href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                    />
+                  </svg>
+                </a>
               </li>
             </ul>
           </div>
