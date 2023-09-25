2f3f7a674b1db974f12f5ccbbe06ab92dc6e7258
otvertka <101932382+otvertka@users.noreply.github.com>
2023-09-25 15:11:26 +0300

    Advantages (#1)

--- src/components/UI/Layout/BestWinterModels.jsx
+++ src/components/UI/Layout/BestWinterModels.jsx
@@ -4,20 +4,10 @@ import Splash_l from "../images/BestWinterMod-img/splash_left-removebg-preview.p
 const BestWinterModels = (props) => {
   return (
     <section className="relative bg-black w-full flex">
-      <h2 className="uppercase absolute pt-20 z-20 text-white text-5xl w-1/4">
-        Лучшие модели для зимней езды
-      </h2>
+      <h2 className="uppercase absolute pt-20 z-20 text-white text-4xl w-1/4">Лучшие модели для зимней езды</h2>
       <div className="flex justify-around w-full">
-        <img
-          className="z-0 opacity-50 object-cover"
-          src={Splash_l}
-          alt="splash-l"
-        />
-        <img
-          className="z-0 opacity-50 object-cover hidden sm:block"
-          src={Splash_r}
-          alt="splash-r"
-        />
+        <img className="z-0 opacity-50 object-cover" src={Splash_l} alt="splash-l" />
+        <img className="z-0 opacity-50 object-cover hidden sm:block" src={Splash_r} alt="splash-r" />
       </div>
     </section>
   );
