0356b6cb1e2e91a05334798695ffcae253253087
otvertka <otvertka123@ukr.net>
2023-09-28 16:24:32 +0300

    Mountain

--- src/components/Layout/MoutainBikes.jsx
+++ src/components/Layout/MoutainBikes.jsx
@@ -8,7 +8,28 @@ const MoutainBikes = () => {
     <section>
       <h2 className="py-11 uppercase text-[40px]">Горные велосипеды</h2>
       <div className="lg:flex justify-between">
-        <div>
+        <div className="w-full max-w-5xl p-5 pb-10 mx-auto columns-2">
+          <a className=" p-5 inline-block rounded relative flex-col bg-gray-700" href="#">
+            <div className="flex justify-between">
+              <h3 className="text-white">Рама</h3>
+              <BikeSVG />
+            </div>
+            <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of</p>
+          </a>
+          <a className="p-5 inline-block rounded relative flex-col bg-gray-700" href="#">
+            <div className="flex justify-between">
+              <h3 className="text-white">Рама</h3>
+              <BikeSVG />
+            </div>
+            <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of</p>
+          </a>
+          <a className="p-5 inline-block rounded relative flex-col bg-gray-700" href="#">
+            <div className="flex justify-between">
+              <h3 className="text-white">Рама</h3>
+              <BikeSVG />
+            </div>
+            <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of</p>
+          </a>
           <a className="p-5 inline-block rounded relative flex-col bg-gray-700" href="#">
             <div className="flex justify-between">
               <h3 className="text-white">Рама</h3>
@@ -18,9 +39,9 @@ const MoutainBikes = () => {
           </a>
         </div>
 
-        <div>
-          <img src={mountBikerImage} alt="mountain_biker" />
-          <p>Горный велосипед несмотря на свое название часто и активно используются в условиях города, так как обладает рядом характеристик, позволяющих сделать велопрогулку максимально приятной и комфортной</p>
+        <div className="">
+          <img className="object-cover" src={mountBikerImage} alt="mountain_biker" />
+          <p className="">Горный велосипед несмотря на свое название часто и активно используются в условиях города, так как обладает рядом характеристик, позволяющих сделать велопрогулку максимально приятной и комфортной</p>
           <button className="bg-orange-500">Каталог</button>
         </div>
       </div>
