ef65d991bdc80d9278c41f780dc3800b1457b78d
otvertka <otvertka123@ukr.net>
2023-10-05 17:40:03 +0300

    carousel

--- src/components/Cards/BikesCard.jsx
+++ src/components/Cards/BikesCard.jsx
@@ -1,32 +1,28 @@
-// import bikesImage from "../../Assets/images/bikes-img/image 23.png";
-// import canadianFlag from "../../Assets/images/flags-img/image 59.png";
-
-// const bike = {
-//   id: 1,
-//   name: "Trek Marlin 7 Matte Nautical Navy Matte Anth ATB 29 2022",
-//   price: 1000,
-//   img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx6iXgi3ehJXPSlmeb8trHZDElzR5-VwQDyJA-QdJSLZM9HArjW00vefX5De-MNcn1tJk&usqp=CAU",
-//   flag: "../src/data/countryImages/italy.png",
-// };
-
-import React from "react";
-
-// import italyFlag from "../../data/countryImages/italy.png";
+import React, { useState } from "react";
+import OneclickSVG from "../UI/svg-components/OneclickSVG";
 
 const BikesCard = ({ bikesInfo }) => {
   const { id, title, country, image, price } = bikesInfo;
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="bg-white rounded-md border-2 border-black mt-22 min-h-max w-1/3 flex flex-col justify-around">
+    <div className="bg-white rounded-md border-2 border-black  flex flex-col justify-around max-w-sm" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <div className="flex justify-between">
         <img height={46} width={88} src={country} alt="flag" />
-        <span>В наличии</span>
+        <span className="text-green-600 m-4">В наличии</span>
       </div>
-      <img src="italy.png" alt="photo-bike" />
-      <h3>{title}</h3>
-      <p>{price}</p>
-
-      <button className="bg-orange-500 rounded mx-1">В 1 клик</button>
+      <div className="flex flex-col justify-center m-4">
+        <img className="" src={image} alt="photo-bike" />
+        <h3 className="text-lg mb-6 ">{title}</h3>
+        <p className="text-gray-600 mb-8">{price} €</p>
+        {isHovered && (
+          <button className="bg-orange-500 rounded-lg py-4 text-white ">
+            <div className="flex justify-center gap-1">
+              <OneclickSVG /> В 1 клик
+            </div>
+          </button>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Layout/NewBikes.jsx
+++ src/components/Layout/NewBikes.jsx
@@ -1,20 +1,45 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 import BikesCard from "../Cards/BikesCard";
 
 import bikesData from "../../data/bikes.json";
 
-const NewBikes = (props) => {
+const NewBikes = (bikesInfo) => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div className="flex flex-col w-full bg-gray-100">
+    <div className=" w-full bg-gray-100">
       <div>
         <h2 className="py-11 uppercase text-6xl">Новинки</h2>
-        {bikesData.map((bike) => (
-          <BikesCard key={bike.id} bikesInfo={bike} />
-        ))}
+        <Carousel responsive={responsive}>
+          {bikesData.map((bike) => (
+            <BikesCard key={bike.id} bikesInfo={bike} />
+          ))}
+        </Carousel>
       </div>
 
-      <a className="pt-12  w-1/3 uppercase justify-items-center" href="#">
-        Показать все
-      </a>
+      <div className="flex justify-center mt-6">
+        <button className="uppercase align-items-center">Показать все</button>
+      </div>
     </div>
   );
 };
