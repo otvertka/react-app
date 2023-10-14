f6cebbaaa189bb78b01b2495a952c8cb5b0c80d3
otvertka <otvertka123@ukr.net>
2023-10-14 17:27:11 +0300

    catalog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
+// import AppRouter from "./components/AppRouter";
 import AppWrapper from "./components/Layout/AppWrapper";
-//
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Admin from "./pages/Admin";
+
+// const router = createBrowserRouter([{ path: "/", element: <Admin /> }]);
 
 function App() {
   return (
