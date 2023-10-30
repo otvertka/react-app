def608e185f5e9456fc447977b5e46a6bc95599d
otvertka <otvertka123@ukr.net>
2023-10-30 15:58:29 +0200

    device page

--- src/components/Cards/BikesCard.jsx
+++ src/components/Cards/BikesCard.jsx
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import OneclickSVG from "../UI/svg-components/OneclickSVG";
 
 const BikesCard = ({ bikesInfo }) => {
-  const { id, title, country, image, price } = bikesInfo;
+  // const { id, title, country, image, price } = bikesInfo;
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="bg-white rounded-md border-2 border-black  flex flex-col justify-around max-w-sm" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <div className="flex justify-between">
-        <img height={46} width={88} src={country} alt="flag" />
+        <img height={46} width={88} src={bikesInfo.country} alt="flag" />
         <span className="text-green-600 m-4">В наличии</span>
       </div>
       <div className="flex flex-col justify-center m-4">
-        <img className="" src={image} alt="photo-bike" />
-        <h3 className="text-lg mb-6 ">{title}</h3>
-        <p className="text-gray-600 mb-8">{price} €</p>
+        <img className="" src={bikesInfo.image} alt="photo-bike" />
+        <h3 className="text-lg mb-6 ">{bikesInfo.title}</h3>
+        <p className="text-gray-600 mb-8">{bikesInfo.price} €</p>
         {isHovered && (
           <button className="bg-orange-500 rounded-lg py-4 text-white ">
             <div className="flex justify-center gap-1">
--- src/pages/BikesPage.jsx
+++ src/pages/BikesPage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import BikesCard from "../components/Cards/BikesCard";
+
+import bikesData from "../data/bikes.json";
+import DefaultAccordion from "../components/Layout/DefaultAccordion";
 
 const BikesPage = () => {
-  return <h1>ВСЕ ВЕЛОСИПЕДЫ</h1>;
+  return (
+    <section>
+      <h1>ВСЕ ВЕЛОСИПЕДЫ</h1>
+      <div className="flex justify-between">
+        <div className="w-1/4">
+          <DefaultAccordion />
+        </div>
+
+        <div className="3/4">
+          <div className="grid grid-cols-3 ">
+            {bikesData.map((bike) => (
+              <BikesCard key={bike.id} bikesInfo={bike} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default BikesPage;
