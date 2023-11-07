63a3000eede336033ee72aed821e7fc4942b21e9
otvertka <otvertka123@ukr.net>
2023-11-07 17:33:34 +0200

    store context

--- src/components/Layout/DefaultAccordion.jsx
+++ src/components/Layout/DefaultAccordion.jsx
@@ -57,6 +57,12 @@ import { Context } from "../..";
 const DefaultAccordion = observer(() => {
   const { bikes } = useContext(Context);
   console.log(bikes);
+
+  const handleTypeChange = (type) => {
+    console.log("Выбран тип:", type.name);
+    bikes.setSelectedType(type);
+  };
+
   return (
     <Accordion>
       <Accordion.Panel>
@@ -65,14 +71,8 @@ const DefaultAccordion = observer(() => {
         <Accordion.Content>
           <ul>
             {bikes.types.map((type) => (
-              <li
-                key={type.id}
-                // active={type.id === bikes.setSelectedType.id}
-                onClick={() => {
-                  console.log("Выбран тип:", type.name);
-                  bikes.setSelectedType(type);
-                }}
-                className={`cursor-pointer ${type.id === bikes.setSelectedType.id ? "text-yellow-300" : ""}`}>
+              <li key={type.id} active={type.id === bikes.setSelectedType.id} className="cursor-pointer">
+                <input type="checkbox" checked={type.id === bikes.selectedType.id} onChange={() => handleTypeChange(type)} />
                 {type.name}
               </li>
             ))}
@@ -82,6 +82,21 @@ const DefaultAccordion = observer(() => {
       <Accordion.Panel>
         <Accordion.Title>Бренд</Accordion.Title>
         <Accordion.Content>
+          <ul>
+            {bikes.brands.map((brand) => (
+              <li
+                className={brand.id === bikes.selectedBrand.id ? "border border-red-800" : ""}
+                key={brand.id}
+                onClick={() => {
+                  bikes.setSelectedBrand(brand);
+                  console.log("Выбран бренд:", brand.name);
+                  console.log(brand.id);
+                  console.log(bikes.selectedBrand.name);
+                }}>
+                {brand.name}
+              </li>
+            ))}
+          </ul>
           {/* <ul>
             {items.brands.map((brand) => (
               <li key={brand.index}>
@@ -95,6 +110,21 @@ const DefaultAccordion = observer(() => {
       <Accordion.Panel>
         <Accordion.Title>Материал рамы</Accordion.Title>
         <Accordion.Content>
+          <ul>
+            {bikes.frames.map((frame) => (
+              <li
+                className={frame.id === bikes.selectedFrame.id ? "border border-red-800" : ""}
+                key={frame.id}
+                onClick={() => {
+                  bikes.setSelectedFrame(frame);
+                  console.log("Выбран тип рамы:", frame.name);
+                  console.log(frame.id);
+                  console.log(bikes.selectedFrame.name);
+                }}>
+                {frame.name}
+              </li>
+            ))}
+          </ul>
           {/* <ul>
             {items.frames.map((frame) => (
               <li key={frame.index}>
--- src/store/BikesStore.js
+++ src/store/BikesStore.js
@@ -15,6 +15,9 @@ export default class BikesStore {
       { id: 3, name: "Ciclistino" },
       { id: 4, name: "Cipollini" },
       { id: 5, name: "Colnago" },
+      { id: 6, name: "Giant" },
+      { id: 7, name: "Pinarello" },
+      { id: 8, name: "Orbea" },
     ];
     this._frames = [
       { id: 1, name: "Алюминий" },
@@ -22,11 +25,15 @@ export default class BikesStore {
       { id: 3, name: "Сталь" },
     ];
     this._bikes = [
-      { id: 1, type: "Велосипеды для триатлона", brand: "Bianci", frame: "Алюминий", title: "AQUILA L DURA ACE DI2 TEAM JUMBO 2021", price: 100, image: "https://kinderfahrzeugwelt.de/wp-content/uploads/2021/02/8900481-1.jpg" },
-      { id: 2, type: "Горные велосипеды", brand: "BMC", frame: " Карбон", title: "BAQUILA L DURA ACE DI2 TEAM JUMBO 2021", price: 200, image: "https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dw524f4c48/images/full/full_2023_/2023/full_2023_3285_pathlite-4_bu_P5.jpg?sw=1300&sfrm=png&q=90&bgcolor=F2F2F2" },
-      { id: 3, type: "Городские велосипеды", brand: "Ciclistino", frame: "Сталь", title: "CAQUILA L DURA ACE DI2 TEAM JUMBO 2021", price: 300, image: "https://lenta.servicecdn.ru/globalassets/1/-/60/23/03/402664_2.png?preset=fulllossywhite" },
+      { id: 1, type: "Велосипеды для триатлона", brand: "Bianci", frame: "Алюминий", title: "BIANCHI NITRON 9.2 XT/SLX 1X12 ", price: 100, image: "../../images/allBikes/Bianci/b1.png", country: "../../images/flags/italy.png" },
+      { id: 2, type: "Горные велосипеды", brand: "Bianci", frame: "Карбон", title: "BIANCHI OLTRE XR4 CV ULTEGRA DI2 12S R400", price: 200, image: "../../images/allBikes/Bianci/b2.png" },
+      { id: 3, type: "Городские велосипеды", brand: "Bianci", frame: "Сталь", title: "BIANCHI OLTRE RC DURA ACE DI2 12S ", price: 300, image: "../../images/allBikes/Bianci/b3.png" },
+      { id: 4, type: "Гравийные велосипеды", brand: "Bianci", frame: "Алюминий", title: "BIANCHI ARCADEX GRX810 DI2 ", price: 400, image: "../../images/allBikes/Bianci/b4.png" },
+      { id: 5, type: "Двухподвесные велосипеды", brand: "Bianci", frame: "Карбон", title: "BIANCHI AQUILA L DURA ACE DI2 TEAM JUMBO 2021", price: 500, image: "../../images/allBikes/Bianci/b5.png" },
     ];
     this._selectedType = {};
+    this._selectedBrand = {};
+    this._selectedFrame = {};
     // this._color = []
     makeAutoObservable(this);
   }
@@ -72,7 +79,7 @@ export default class BikesStore {
   }
 
   get selectedBrand() {
-    return this._selectedFrame;
+    return this._selectedBrand;
   }
   get selectedFrame() {
     return this._selectedFrame;
