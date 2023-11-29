9b43321a1e333937188e0fcd105ebf2ba1e9b69e
otvertka <otvertka123@ukr.net>
2023-11-29 11:37:10 +0200

    cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,30 +1,28 @@
 import Modal from "../components/Layout/Modal";
 import { useContext } from "react";
 import CartContext from "../store/CartContext";
-import UserProgressContext from "../store/UserProgressContext";
+import Counter from "../components/Counter";
+// import UserProgressContext from "../store/UserProgressContext";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
   const cartTotal = cartCtx.items.reduce((tottalPrice, item) => tottalPrice + item.quantity * item.price, 0);
 
-  const userProgressCtx = useContext(UserProgressContext);
+  // const userProgressCtx = useContext(UserProgressContext);
 
   return (
-    <Modal open={userProgressCtx.progress === "cart"}>
-      <h2>Your cart</h2>
-
-      <ul>
-        {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <p>{cartTotal}</p>
-      <p>
-        <button textOnly>Close</button>
-        <button>Go to checkout</button>
-      </p>
-    </Modal>
+    <section className="">
+      <h1 className="uppercase text-6xl font-semibold mt-32">Корзина</h1>
+      <section className="mt-20 border">
+        <ul className="flex flex-col">
+          {cartCtx.items.map((item) => (
+            <li key={item.id}>
+              <img src={item.image} alt="img" className="inline-block" /> {item.title} <Counter /> {item.quantity} {item.price}
+            </li>
+          ))}
+        </ul>
+        <p>{cartTotal}</p>
+      </section>
+    </section>
   );
 }
