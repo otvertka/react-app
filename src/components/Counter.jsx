ef9a3866cf4a80b8e5c8aedb51a58673078fd177
otvertka <otvertka123@ukr.net>
2023-12-01 17:10:36 +0200

    counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import counterStore from "../store/counter";
-import { observer } from "mobx-react-lite";
+import React, { useState } from "react";
 
-const Counter = observer(() => {
+const Counter = ({ updateQuant }) => {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount(count + 1);
+    updateQuant(count + 1);
+  };
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+      updateQuant(count - 1);
+    }
+  };
   return (
     <div className="border flex justify-between">
-      <button className="inline-block px-4 py-2 cursor-pointer" onClick={() => counterStore.decrement()}>
+      <button className="inline-block px-4 py-2 cursor-pointer" onClick={decrement}>
         -
       </button>
-      <span className="text-xl pt-2">{counterStore.count}</span>
-      <button className="inline-block px-4 py-2 cursor-pointer" onClick={() => counterStore.increment()}>
+      <span classNames="text-xl">{count}</span>
+      <button className="inline-block px-4 py-2 cursor-pointer" onClick={increment}>
         +
       </button>
     </div>
   );
-});
+};
 
 export default Counter;
+// import React from "react";
+// import counterStore from "../store/counter";
+// import { observer } from "mobx-react-lite";
+
+// const Counter = observer(() => {
+//   return (
+//     <div className="border flex justify-between">
+//       <button className="inline-block px-4 py-2 cursor-pointer" onClick={() => counterStore.decrement()}>
+//         -
+//       </button>
+//       <span className="text-xl pt-2">{counterStore.count}</span>
+//       <button className="inline-block px-4 py-2 cursor-pointer" onClick={() => counterStore.increment()}>
+//         +
+//       </button>
+//     </div>
+//   );
+// });
+
+// export default Counter;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import Modal from "../components/Layout/Modal";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../store/CartContext";
 import Counter from "../components/Counter";
 
@@ -8,7 +8,13 @@ import deleteSvg from "../../src/Assets/svg/deleteSVG.svg";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
-  const cartTotal = cartCtx.items.reduce((tottalPrice, item) => tottalPrice + item.quantity * item.price, 0);
+
+  const [total, setTotal] = useState(0);
+
+  const updateTotal = (quantity, price) => {
+    setTotal(quantity * price);
+  };
+  // const cartTotal = cartCtx.items.reduce((tottalPrice, item) => tottalPrice + item.quantity * item.price, 0);
 
   // const userProgressCtx = useContext(UserProgressContext);
 
@@ -19,24 +25,23 @@ export default function Cart() {
         <ul className="flex flex-col">
           {cartCtx.items.map((item) => (
             <li key={item.id} className="flex flex-col md:flex-row justify-around border-b">
-              <div className="flex justify-between items-center space-x-6">
+              <div className="flex justify-between  space-x-6">
                 <img src={item.image} alt={item.title} className="w-28 h-24 md:h-40 md:w-44 " /> <span className="md:text-lg">{item.title} </span>
               </div>
-              <div className="flex flex-col md:flex-row items-center justify-between">
-                <div className="space-x-6">
-                  <div className="inline-block max-w-[123px] mr-6">
-                    <Counter />
+              <div className="flex flex-col md:flex-row">
+                <div className="">
+                  <div className="inline-block max-w-[123px]">
+                    <Counter updateQuant={(quantity) => updateTotal(quantity, item.price)} />
                   </div>
-                  {item.quantity} {item.price}
+                  <p>{total}</p>
                 </div>
-                <button className="inline-block justify-center ml-5">
+                <button className="inline-block justify-center">
                   <img src={deleteSvg} alt="delete" />
                 </button>
               </div>
             </li>
           ))}
         </ul>
-        <p>Итого:{cartTotal}</p>
       </section>
     </section>
   );
