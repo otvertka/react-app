0652c1e37ccb8118c20f5e7f3b5a318a2d170b1a
otvertka <otvertka123@ukr.net>
2023-12-06 15:20:48 +0200

    Checkout Page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,29 +1,57 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../store/CartContext";
 import CheckoutInput from "../components/UI/CheckoutInput";
 
 export default function CheckoutPage() {
   const cartCtx = useContext(CartContext);
+  const [orderConfirmed, setOrderConfirmed] = useState(false);
+
   const cartTotalPrice = cartCtx.items.reduce((tottalPrice, item) => tottalPrice + item.quantity * item.price, 0);
   const cartTotalQuantity = cartCtx.items.reduce((totalQuantity, item) => totalQuantity + item.quantity, 0);
   console.log(cartCtx);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries()); //
+
+    const orderData = {
+      customer: customerData,
+      items: cartCtx.items,
+      tottalPrice: cartTotalPrice,
+    };
+
+    localStorage.setItem("orderData", JSON.stringify(orderData));
+
+    setOrderConfirmed(true);
+  }
+
+  if (orderConfirmed) {
+    return <>Заказ подтверждён</>;
+  }
+
   return (
     <section className="mt-20">
       <h1 className="uppercase text-5xl font-medium m-5">Оформление заказа</h1>
       <div className="flex flex-col md:flex-row md:justify-between mx-10">
         <div className="">
-          <h2 className="uppercase text-xl font-semibold mb-4">Покупатель</h2>
-          <CheckoutInput label="Name*" type="text" id="name" placeholder="Имя" />
-          <CheckoutInput label="Surname*" type="text" id="surname" placeholder="Фамилия" />
-          <CheckoutInput label="E-mail adress*" type="email" id="email" placeholder="e-mail" />
-          <div className="flex">
-            <CheckoutInput label="Street*" type="text" id="street" placeholder="Улица" />
-            <CheckoutInput label="House number*" type="text" id="house" placeholder="Дом" />
-          </div>
-          <div className="flex">
-            <CheckoutInput label="City*" type="text" id="city" placeholder="Город" />
-            <CheckoutInput label="Postal Code*" type="text" id="postal-code" placeholder="Почтовый индекс" />
-          </div>
+          <form onSubmit={handleSubmit}>
+            <h2 className="uppercase text-xl font-semibold mb-4">Покупатель</h2>
+            <CheckoutInput label="Name*" type="text" id="name" placeholder="Имя" />
+            <CheckoutInput label="Surname*" type="text" id="surname" placeholder="Фамилия" />
+            <CheckoutInput label="E-mail*" type="email" id="email" placeholder="e-mail" />
+            <div className="flex">
+              <CheckoutInput label="Street*" type="text" id="street" placeholder="Улица" />
+              <CheckoutInput label="House number*" type="text" id="house" placeholder="Дом" />
+            </div>
+            <div className="flex">
+              <CheckoutInput label="City*" type="text" id="city" placeholder="Город" />
+              <CheckoutInput label="Postal Code*" type="text" id="postal-code" placeholder="Почтовый индекс" />
+            </div>
+            <button type="submit" className="px-5 w-full h-12 rounded-lg bg-orange-600 hover:bg-orange-500 active:bg-orange-700 hover:scale-105 transition-all duration-300 text-white">
+              Подтвердить заказ
+            </button>
+          </form>
         </div>
         <div className="">
           <h2 className="uppercase text-3xl font-bold mb-11">ВАШ ЗАКАЗ </h2>
@@ -49,7 +77,6 @@ export default function CheckoutPage() {
           <h3 className="text-xl font-bold mb-2">
             Общая сумма: <span className="font-semibold">{cartTotalPrice}</span> €
           </h3>
-          <button className="px-5 w-full h-12 rounded-lg bg-orange-600 hover:bg-orange-500 active:bg-orange-700 hover:scale-105 transition-all duration-300 text-white">Подтвердить заказ</button>
         </div>
       </div>
     </section>
