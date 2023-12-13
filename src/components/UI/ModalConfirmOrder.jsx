21a0a80421be9914846a5174d0ce638f04468fe4
otvertka <otvertka123@ukr.net>
2023-12-13 15:48:24 +0200

    checkout_modal

--- src/components/UI/ModalConfirmOrder.jsx
+++ src/components/UI/ModalConfirmOrder.jsx
@@ -1,8 +1,8 @@
 import Modal from "./Modal";
 
-export default function ModalConfirmOrder() {
+export default function ModalConfirmOrder({ onClose }) {
   return (
-    <Modal className="w-full h-full bg-white" open={true}>
+    <Modal className="w-full h-full bg-white" open={true} onClose={onClose}>
       <h2 className="text-5xl">Ваш заказ подтверждён</h2>
       <button>Отследить заказ</button>
     </Modal>
